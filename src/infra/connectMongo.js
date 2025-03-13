361effc192b4a6b3f8595731b989c9d44bf9846b
Rachel Braga <rachel.braga@flipsaude.com.br>
2025-03-12 23:10:44 -0300

    ajuste conexao com banco

--- src/infra/connectMongo.js
+++ src/infra/connectMongo.js
@@ -1,17 +1,12 @@
-import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const uri = process.env.MONGO_URI;
 
-const client = new MongoClient(uri);
+mongoose.connect(uri);
 
-try {
-    await client.connect();
-    console.log("Conectado ao MongoDB com sucesso!");
-} catch (error) {
-    console.error("Erro na conexão com o MongoDB:", error);
-}
+const mongodb = mongoose.connection;
 
-export default client;
+export default mongodb;
