8c0ea90415729b3c56bba8c9ece417451134a590
Rachel Braga <rmbraga@me.com>
2025-03-13 00:00:00 -0300

    voltando a condig de cors

--- src/server/index.js
+++ src/server/index.js
@@ -8,11 +8,7 @@ dotenv.config();
 const app = express(); 
 
 app.use(express.json());
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors({origin:"*"}));
 routes(app)
 mongodb.on("error", console.log.bind(console, "Erro de conexão"))
 mongodb.once("open", () => {
